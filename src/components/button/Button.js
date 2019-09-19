ba8e4c798e3d12a0a08a49484b4db8e49a6f3672
Rafael Fazolli <rafael.fazolli@dextra-sw.com>
2019-09-19 16:47:19 -0300

    create styled components

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './Button.scss';
+
+const Botao = styled.button`
+    width: 100%;
+    height: 50px;
+    border: 0;
+    font-size: 20px;
+    font-weight: 100;
+    background: green;
+    transition: all ease-in-out .3s;
+    cursor: pointer;
+`;
 
 const Button = (props) => {
     return (
-        <button className="button" onClick={ props.buttonAction }>{ props.buttonText }</button>
+        <Botao onClick={ props.buttonAction }>{ props.buttonText }</Botao>
     )
 }
 
